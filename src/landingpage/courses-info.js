b3b9c45dedddd3c3868f9bceee16ff5c7f732833
Alan Hurtarte <alan@mypostcard.com>
2019-11-23 17:54:58 -0600

    Added more information

--- src/landingpage/courses-info.js
+++ src/landingpage/courses-info.js
@@ -5,33 +5,146 @@ class Coursesinfo extends React.Component {
             <div className="section container">
                 <h2>Acerca de los cursos</h2>
                 <div className="row">
-                    <div className="col-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div className="col-6">
+                        <h3>Objetivos del curso</h3>
+                        <p className="text-left">Este curso tiene como principal objetivo dar al participante los conocimientos de las técnicas más útiles
+                            que serán capaces de aplicar inmediatamente después del curso para incrementar la calidad de su
+                            trabajo, especialmente en cuanto a función y estética, pero siempre de una forma sencilla.</p>
+                    </div>
+                    <div className="col-6">
+                        <img src="/svg/undraw_detailed_analysis_xn7y.svg" alt="" className="img-fluid" />
+                    </div>
+                    <div className="col-6">
+                        <img src="/svg/undraw_windy_day_x63l.svg" alt="" className="img-fluid" />
+                    </div>
+                    <div className="col-6 my-5">
+                        <h3>El participante aprenderá:</h3>
+                        <ul className="text-left my-4 mx-auto" style={{ 'max-width': '400px' }}>
+                            <li>Diseño de sonrisa digital DSD.</li>
+                            <li>Fotografia & video en odontología.</li>
+                            <li>Mock Up | Maqueta | Prototipo | APT</li>
+                            <li>Preparaciones para carillas Central, Lateral, Canino, Premolar</li>
+                            <li>Técnica de Inyección en sector anterior y posterior</li>
+                        </ul>
+                    </div>
+                    <div className="py-4 my-5"><br /></div>
+                    <div className="col-12 mt-5 pt-4">
+                        <h3>Día 1</h3>
+                    </div>
+                    <div className="col-12 mt-4">
+                        <div className="row">
+                            <div className="col-1"></div>
+                            <div className="col-5 ml-auto">
+                                <div class="card">
+                                    <div class="card-body text-left">
+                                        <h5 class="card-title">Primera parte</h5>
+                                        <h6>Diseño Sonrisa Digital</h6>
+                                        <p class="card-text">
+                                            Concepto. <br />
+                                            Protocolo fotografico.<br />
+                                            Protocolo de video.<br />
+                                            Planeación.<br />
+                                            Flujo de trabajo digital.<br />
+                                            Planos de referencia.<br />
+                                            Plano oclusal.<br />
+                                            Arco facial digital.<br />
+                                            Plano horizontal.<br />
+                                            Plano vertical.<br />
+                                            Visagismo.<br />
+                                            Proporción aurea.<br />
+                                            Regla digital uso y manejo.<br />
+                                            Comunicación interdisciplinaria.<br />
+                                            Mock up: Motivacional, substractivo, ideal.<br />
+                                            <br />
+                                            <br />
+                                        </p>
+                                        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                                    </div>
+                                </div>
                             </div>
+                            <div className="col-5 mr-auto">
+                                <div class="card">
+                                    <div class="card-body text-left">
+                                        <h5 class="card-title">Segunda parte</h5>
+                                        <h6>Fotografia Video Dental</h6>
+                                        <p class="card-text">
+                                            Protocolo fotografico completo: 23 fotografias. <br />
+                                            Protocolo de video completo.<br />
+                                            Equipo basico y avanzado.<br />
+                                            Instrumental para toma fotografica.<br />
+                                            Camara DSLR clasificación y modelos.<br />
+                                            Tipos de lentes.<br />
+                                            Tipos de flash.<br />
+                                            Estudio fotografico.<br />
+                                            Iluminación LED.<br />
+                                            Soportes y tripodes.<br />
+                                            Accesorios para edición.<br />
+                                            Conceptos básicos de fotografia.<br />
+                                            Tiempo de Exposición, apertura, ISO.<br />
+                                            Balance de blancos WB.<br />
+                                            Configuración basica & avanzada para fotografia.<br />
+                                            Configuración basica & avanzada para video.<br />
+                                            y mas...
+                                        </p>
+                                        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-1"></div>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                    <div className="col-12 mt-5 pt-4">
+                        <h3>Día 2</h3>
+                    </div>
+                    <div className="col-12 mt-4">
+                        <div className="row">
+                            <div className="col-1"></div>
+                            <div className="col-5 ml-auto">
+                                <div class="card">
+                                    <div class="card-body text-left">
+                                        <h5 class="card-title">Introducción Teoría</h5>
+                                        <h6>Flowable Injection Technique</h6>
+                                        <p class="card-text">
+                                            Comunicación y entrevista con paciente. <br />
+                                            Fotografía y video. <br />
+                                            Análisis Estético. <br />
+                                            Diseño de Sonrisa. <br />
+                                            Toma de Impresión. <br />
+                                            Articuladores. <br />
+                                            Arcos faciales. <br />
+                                            Relación céntrica. <br />
+                                            Oclusión céntrica. <br />
+                                            Y mas...
+                                        </p>
+                                        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                                    </div>
+                                </div>
                             </div>
+                            <div className="col-5 mr-auto">
+                                <div class="card">
+                                    <div class="card-body text-left">
+                                        <h5 class="card-title">Hands-onPractica</h5>
+                                        <h6>Flowable Injection Technique</h6>
+                                        <p class="card-text">
+                                            Preparaciones para carilla tipo cerámica clasificación I,II,III. <br />
+                                            Cubeta transparente como realizarla paso a paso. <br />
+                                            Silicona transparente uso y diferencias en marcas. <br />
+                                            Detallado y acabo de silicona transparente. <br />
+                                            Descontaminación de material para mejor adhesion. <br />
+                                            Protocolo adhesivo. <br />
+                                            Inyección de resina. <br />
+                                            Acabado y terminado. <br />
+                                            Pulido y alto brillo. <br />
+                                        </p>
+                                        {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-1"></div>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
             </div >
         );
--- src/landingpage/header.js
+++ src/landingpage/header.js
@@ -5,26 +5,28 @@ class Header extends React.Component {
             <div className="header">
                 <div className="background bg-3" alt="Photo by The Climate Reality Project on Unsplash"></div>
                 <div className="logo">
-                    <h1>Odonto MeetUp</h1>
-                    <h2>THE PEOPLES WEB DESIGN CONFERINCE</h2>
+                    <h1>Flowable Injection Technique</h1>
+                    <h2>Enrique Díaz - Odontología Independiente</h2>
                 </div>
                 <div className="event-info">
                     <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4">
                         <li>
-                            <i class="fa fa-map-marker "></i>London, England
+                            <i class="fa fa-map-marker "></i> Ciudad de Guatemala
 						</li>
                         <li>
-                            <i class="fa fa-calendar"></i>25/08/2020
+                            <i class="fa fa-calendar"></i>25/05/2020
 						</li>
                         <li>
-                            <i class="fa fa-microphone"></i>8 Speakers
+                            <i class="fa fa-microphone"></i>8 Expositores
 						</li>
                         <li>
-                            <i class="fa fa-ticket"></i>107 tickets left
+                            <i class="fa fa-ticket"></i>107 tickets restantes
 						</li>
                     </ul>
-                    <h1>3 days of inspiration and education on user experience.</h1>
-                    <h2>On April 23rd 2014 in Newcastle upon Tyne, UK, Industry is back with another carefully curated selection of talks. <br /> 8 Speakers will provide insights in to their work and offer ways to make us all better at what we do.</h2>
+                    <h1>2 dias para adquirir conocimientos de las técnicas más útiles.</h1>
+                    <h2>Técnicas que serán capaces de aplicar inmediatamente después del curso para incrementar la calidad de su trabajo,
+                        <br/> especialmente en cuanto a función y estética, pero siempre de una forma sencilla.
+                    </h2>
                 </div>
 
             </div>
