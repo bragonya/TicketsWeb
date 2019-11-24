5a263ca5fc21dda38b91ff709b15b0064a2aa5f5
fernando <rluis4490@gmail.com>
2019-11-23 22:29:14 -0600

    seats croquis integrated

--- src/App.js
+++ src/App.js
@@ -9,66 +9,17 @@ import {
 import { } from "./App.scss"
 
 import LandingPage from "./landingpage/landingpage"
+import MainStage from "./components/main-stage/main-stage.component";
+
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-expand-md fixed-top">
-          <div className="row navbar-wrapper">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <Link to="/" className="nav-link">Home</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/about" className="nav-link">About</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/users" className="nav-link">Users</Link>
-                </li>
-              </ul>
-              <Link to="/register" className="btn btn-orange">Registro</Link>
-            </div>
-          </div>
-        </nav>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/pricing">
-            <Pricing />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-
-        <footer className="footer">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <Link to="/about">Acerca de</Link>
-                <Link to="/pricing">Precios</Link>
-                <Link>© Copyright. All Rights Reserved 2019.</Link>
-              </div>
-            </div>
-          </div>
-        </footer >
-      </div>
-    </Router>
-  );
+    <MainStage
+      onSelectSeat={seatId => {
+        console.log("selected - " + seatId);
+      }}
+    />)
+    
 }
 
 function Home() {
