77ba47eec9d2d0f930eb978d4c5e9254e0fa120a
fernando <rluis4490@gmail.com>
2019-11-24 20:03:54 -0600

    responsive croquis

--- src/components/croquis/croquis.component.jsx
+++ src/components/croquis/croquis.component.jsx
@@ -3,7 +3,7 @@ import './croquis.styles.scss';
 import Section from '../section/new-section.component';
 const Croquis = () =>(
     <div className="container">
-	    <div className="box box-1">ESCENARIO
+	    <div className="box box-1">
             <div className="grid-row">
                 <Section title='Sala Lounge' rows={['A','B']} cols={Array.from({length:5},(v,k)=>k+1)}/>
                 <hr></hr>
@@ -25,6 +25,18 @@ const Croquis = () =>(
                 <Section title='Profesionales' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
                 <hr/>
                 <Section title='Profesionales' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
+                    
+            </div>
+            <hr/>
+            <div className="grid-row">
+                <Section title='Estudiantes' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
+                <hr/>
+                <Section title='Estudiantes' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
+
+                <hr/><hr/>
+                <Section title='Estudiantes' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
+                <hr/>
+                <Section title='Estudiantes' rows={['A','C','E','G','I','K','M','O','Q']} cols={Array.from({length:8},(v,k)=>k+1)}/>
                     
             </div>
             
