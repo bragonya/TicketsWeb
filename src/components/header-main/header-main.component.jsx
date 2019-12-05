fdad607dd9092d0aec55b06ced231774007c0a65
bragonya <bragonya@gmail.com>
2019-12-05 13:37:50 -0600

    hide toogle when tap

--- src/components/header-main/header-main.component.jsx
+++ src/components/header-main/header-main.component.jsx
@@ -3,30 +3,34 @@ import { Link } from 'react-router-dom';
 const HeaderMain = () =>(
     <nav className="navbar navbar-expand-md fixed-top">
     <div className="row navbar-wrapper">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <button id = "toogleButton" className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/" className="nav-link" onClick={() => collapseClick()}>Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link">About</Link>
+            <Link to="/about" className="nav-link" onClick={() => collapseClick()}>About</Link>
           </li>
           <li className="nav-item">
-            <Link to="/users" className="nav-link">Users</Link>
+            <Link to="/users" className="nav-link" onClick={() => collapseClick()}>Users</Link>
           </li>
           <li className="nav-item">
-            <Link to="/reservation" className="nav-link">Reservation</Link>
+            <Link to="/reservation" className="nav-link" onClick={() => collapseClick()}>Reservation</Link>
           </li>
           <li className="nav-item">
-            <Link to="/register" className="nav-link btn btn-orange">Registro</Link>
+            <Link to="/register" className="nav-link btn btn-orange" onClick={() => collapseClick()}>Registro</Link>
           </li>
         </ul>
       </div>
     </div>
   </nav>
 );
+
+const collapseClick = () => {
+  document.getElementById("toogleButton").click();
+}
 
 export default HeaderMain;
