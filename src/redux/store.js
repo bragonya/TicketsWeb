badeea82fa10857eca7e7b3a7e04edc2e2397780
fernando <rluis4490@gmail.com>
2019-12-19 23:53:17 -0600

    local storage cart items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger,thunk];
+const middlewares = [/*logger,*/thunk];
 
 const store = createStore(rootReducer,applyMiddleware(...middlewares));
 
