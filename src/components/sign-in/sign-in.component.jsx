7250ca9d5cf754a56ec884b64f93882e0d3ca6b3
fernando <rluis4490@gmail.com>
2019-12-24 00:06:35 -0600

    fixed cors

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,6 +41,7 @@ class SignIn extends React.Component {
             if(!(fullnameSplit[0].trim()==="") && !(fullnameSplit[2].trim()==="")){
                 fetch(process.env.REACT_APP_BASE_URL + "/login", {
                     method: "post",
+                    mode: 'no-cors',
                     headers: {
                         'Accept': 'application/json',
                         'Content-Type': 'application/json'
