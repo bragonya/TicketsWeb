d273e364438729f220c555b2c66e567375c5fb9e
fernando <rluis4490@gmail.com>
2019-12-24 00:04:22 -0600

    fixed cors

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -33,7 +33,7 @@ class SignUp extends React.Component {
         try {
             let response = await fetch(process.env.REACT_APP_BASE_URL + "/register", {
                 method: "post",
-                mode: 'cors',
+                mode: 'no-cors',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
