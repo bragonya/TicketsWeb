fdce24ef18755d147ef9c68ef5c7b3cadaef5d94
fernando <rluis4490@gmail.com>
2019-12-25 17:32:15 -0600

    fixed on change route 2

--- src/App.js
+++ src/App.js
@@ -129,7 +129,6 @@ export class App extends React.Component{
   
 
   componentWillUnmount() {
-    this.unlisten();
     socket.disconnect();
     this.unsubscribeFromAuth();
     localStorage.removeItem('user');
