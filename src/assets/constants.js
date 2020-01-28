4a82b3ac1d99bb0f8b00d795eae8f36fc9b19f2b
fernando <rluis4490@gmail.com>
2020-01-27 19:58:52 -0600

    change prices to seat by course and section

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -20,54 +20,54 @@ export const CONST_PRICES={
     KIM:{
         'only':{
                 LOUNGE:{
-                    PRICE: 1//PRICE: 1800
+                    PRICE: 1800
                 },
                 VIP: {
-                    PRICE: 1//PRICE:1600
+                    PRICE:1600
                 },
                 PROFESIONAL: {
-                    PRICE: 1//PRICE:1450
+                    PRICE:1450
                 }
         },
         'BOTH':{
             LOUNGE:{
-                PRICE: 1//PRICE: 1400
+                PRICE: 1400
             },
             VIP: {
-                PRICE: 1//PRICE:1200
+                PRICE:1200
             },
             PROFESIONAL: {
-                PRICE: 1//PRICE:1050
+                PRICE:1050
             }
         }
     },
     KANO:{
         'only':{
             LOUNGE:{
-                PRICE: 1//PRICE: 1800
+                PRICE: 1800
             },
             VIP: {
-                PRICE: 1//PRICE:1600
+                PRICE:1600
             },
             PROFESIONAL: {
-                PRICE: 1//PRICE:1450
+                PRICE:1450
             },
             ESTUDIANTE: {
-                PRICE: 1//PRICE:750
+                PRICE:750
             }
         },
         'BOTH':{
             LOUNGE:{
-                PRICE: 1//PRICE: 1400
+                PRICE: 1400
             },
             VIP: {
-                PRICE: 1//PRICE:1200
+                PRICE:1200
             },
             PROFESIONAL: {
-                PRICE: 1//PRICE:1050
+                PRICE:1050
             },
             ESTUDIANTE: {
-                PRICE: 1//PRICE:750
+                PRICE:750
             }
         }
     }
