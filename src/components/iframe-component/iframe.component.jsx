fcde2ea5fcfff89dd573407c7f3cc25fa51fd2f8
fernando <rluis4490@gmail.com>
2020-02-06 00:53:13 -0600

    try listening on change url iframe

--- src/components/iframe-component/iframe.component.jsx
+++ src/components/iframe-component/iframe.component.jsx
@@ -12,8 +12,46 @@ export default class IframeComponent extends React.Component {
           loading: false
         });
     }
+    iframeURLChange = (iframe, callback) =>{
+        var lastDispatched = null;
     
+        var dispatchChange = function () {
+            var newHref = iframe.contentWindow.location.href;
     
+            if (newHref !== lastDispatched) {
+                callback(newHref);
+                lastDispatched = newHref;
+            }
+        };
+    
+        var unloadHandler = function () {
+            // Timeout needed because the URL changes immediately after
+            // the `unload` event is dispatched.
+            setTimeout(dispatchChange, 0);
+        };
+    
+        function attachUnload() {
+            // Remove the unloadHandler in case it was already attached.
+            // Otherwise, there will be two handlers, which is unnecessary.
+            iframe.contentWindow.removeEventListener("unload", unloadHandler);
+            iframe.contentWindow.addEventListener("unload", unloadHandler);
+        }
+    
+        iframe.addEventListener("load", function () {
+            attachUnload();
+    
+            // Just in case the change wasn't dispatched during the unload event...
+            dispatchChange();
+        });
+    
+        attachUnload();
+    }
+
+    componentDidMount(){
+        this.iframeURLChange(document.getElementById("iframe-FAC"), function (newURL) {
+            console.log("URL changed:", newURL);
+        });
+    }
     render() {
         const { props: { src, height, width }, state: { loading } } = this;
         return (
@@ -25,7 +63,7 @@ export default class IframeComponent extends React.Component {
                     :
                     null
                 }
-                <iframe
+                <iframe     
                     key='iframe-FAC-Unbiased'
                     id='iframe-FAC' 
                     title="FAC Payment"
