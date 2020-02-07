5f782486f9add3ef20df5b92853a52793661e92a
fernando <rluis4490@gmail.com>
2020-02-07 08:24:50 -0600

    fixed convert time clock

--- src/components/clock/clock.component.jsx
+++ src/components/clock/clock.component.jsx
@@ -14,7 +14,8 @@ const Clock = ({time}) =>(
 
 const separateTime = (time)=>{
     if(time>60){
-        let minutes = Math.floor((time-60) / 60);
+        time = time-60;
+        let minutes = Math.floor(time / 60);
         let seconds = time - minutes * 60;
     
         let timeConverted =`${minutes<10?'0':''}${minutes}:${seconds<10?'0':''}${seconds}`;
